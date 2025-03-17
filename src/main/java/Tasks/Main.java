c0c158b05a2384c191f0d8fc7e78567f7bcd2f32
KostiantynAnanchenko <itemswap@gmail.com>
2025-03-17 21:42:27 +0100

    1

--- src/main/java/Tasks/Main.java
+++ src/main/java/Tasks/Main.java
@@ -1,15 +1,8 @@
 package Tasks;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введіть перше число: ");
-        int a = scanner.nextInt();
-        System.out.print("Введіть друге число: ");
-        int b = scanner.nextInt();
-        Calculator calculator = new Calculator();
-        calculator.calculate(a,b);
+
     }
 }
